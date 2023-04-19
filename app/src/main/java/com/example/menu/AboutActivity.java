8da7b16d5e46d19ffbdb921bcebfdfa7483e1ec1
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-04-19 23:18:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/menu/AboutActivity.java
@@ -0,0 +1,38 @@
+package com.example.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.about){
+            startActivity(new Intent(this, AboutActivity.class));
+        } else if (item.getItemId() == R.id.setting) {
+            startActivity(new Intent(this, SettingActivity.class));
+        } else if (item.getItemId() == R.id.help) {
+            startActivity(new Intent(this, HelpActivity.class));
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.menu;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.about){
+            startActivity(new Intent(this, AboutActivity.class));
+        } else if (item.getItemId() == R.id.setting) {
+            startActivity(new Intent(this, SettingActivity.class));
+        } else if (item.getItemId() == R.id.help) {
+            startActivity(new Intent(this, HelpActivity.class));
+        }
+
+        return true;
+    }
+}
