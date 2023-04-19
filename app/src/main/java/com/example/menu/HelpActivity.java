90013416902101d3803fe9ac6cd7165e25c6de04
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-04-19 23:39:15 +0800

    update

--- app/src/main/java/com/example/menu/HelpActivity.java
+++ app/src/main/java/com/example/menu/HelpActivity.java
@@ -2,7 +2,11 @@ package com.example.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.about){
+            startActivity(new Intent(this, AboutActivity.class));
+        } else if (item.getItemId() == R.id.setting) {
+            startActivity(new Intent(this, SettingActivity.class));
+        } else if (item.getItemId() == R.id.help) {
+            startActivity(new Intent(this, HelpActivity.class));
+        }
+        return true;
     }
 }
